a9f2331a8fd3d829b381e3abb898590920bf800e
WhitePeachTea <penglei2123@gmail.com>
2020-03-03 00:25:54 -0500

    database create

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,15 @@
 -- insert burgers here
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Big Mac", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Cheese Burger", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Thai Sweet Chili Chicken Sandwich", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Filet-O-Fish", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("McChicken", 0);
--- server.js
+++ server.js
@@ -1,1 +1,24 @@
 // placeholder
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser')
+var exphbs = require('express-handlebars');
+var router = require('./controllers/burgers_controller.js');
+var PORT = process.env.PORT || 3036;
+
+
+var app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static('public'));
+
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use('/', router);
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
