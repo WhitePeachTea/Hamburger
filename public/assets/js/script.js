8861f7c770567a0b842fdaeefa44034402f9796f
WhitePeachTea <penglei2123@gmail.com>
2020-03-03 01:11:39 -0500

    version without css

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,1 +1,38 @@
-// jquery code goes here
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".devourButton").on("click", function(event) {
+    console.log(event.target);
+
+      // Send the PUT request.
+      $.ajax("/api/burgers/"+event.target.value,{
+        type: "PUT",
+      }).then(
+        function() {
+          $(`.${event.target.className}`).remove();
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        name: $("#burgerName").val().trim(),
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+    // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
+  
--- server.js
+++ server.js
@@ -1,24 +1,29 @@
-// placeholder
-var path = require('path');
-var express = require('express');
-var bodyParser = require('body-parser')
-var exphbs = require('express-handlebars');
-var router = require('./controllers/burgers_controller.js');
-var PORT = process.env.PORT || 3036;
+var express = require("express");
 
+var PORT = process.env.PORT || 8080;
 
 var app = express();
 
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+// Parse application body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(express.static('public'));
+// Set Handlebars.
+var exphbs = require("express-handlebars");
 
-app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-app.use('/', router);
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgersController.js");
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
