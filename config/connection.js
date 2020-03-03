220fbc7b66f63dce62d426e30959d40808935bd1
WhitePeachTea <penglei2123@gmail.com>
2020-03-03 00:40:07 -0500

    config

---
+++ config/connection.js
@@ -0,0 +1,30 @@
+const mysql = require("mysql");
+
+const connection;
+
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+  } else {
+    connection = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      port: 3306,
+      password: '',
+      database: 'burgers_db'
+    });
+  }
+
+  // Make connection.
+
+  connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+
+
+  // Export connection for our ORM to use.
+
+  module.exports = connection;
