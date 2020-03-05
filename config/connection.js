eb4790f72568447e2ca8cda8d45ad6a5d1022e76
WhitePeachTea <penglei2123@gmail.com>
2020-03-05 00:05:50 -0500

    x

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,9 @@
 var mysql = require("mysql");
 var connection;
 
-if(process.env.JAWSDB_URL){
-    connection = mysql.createConnection(process.env.JAWSDB_URL)
-    connection.connect();
-    connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-      if (err) throw err;
-
-  console.log('The solution is: ', rows[0].solution);
-});
-
-connection.end();
-  } else {
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
     connection = mysql.createConnection({
       host: 'localhost',
       user: 'root',
