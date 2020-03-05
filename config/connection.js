0234ac6425ae8198c656e474194caf1d5d7e05ad
WhitePeachTea <penglei2123@gmail.com>
2020-03-05 18:38:39 -0500

    x

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,18 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+    connection = mysql.createConnection({
+      host: 'u28rhuskh0x5paau.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+      user: 'q4lx0q846fywhovu',
+      password: 'cortm4acd6yvpqoy',
+      database: 'vcgvbphurjnrvd2x',
+      port: 3306
+    });
 } else {
     connection = mysql.createConnection({
       host: 'localhost',
       user: 'root',
-      password: '',
+      password: 'password',
       database: 'burgers_db'
     });
   }
